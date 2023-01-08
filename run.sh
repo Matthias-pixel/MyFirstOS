975aac1dc3016a73e6ff6b326dcf10329a0601c9
Matthias-pixel <vinc.gold01@gmail.com>
2023-01-08 21:45:41 +0100

    Added actual run command to run.sh

--- run.sh
+++ run.sh
@@ -9,3 +9,5 @@ dd if="/dev/zero" of="build/main_floppy.img" bs=512 count=2880;
 mkfs.fat -F 12 -n "MYFIRSTOS" "build/main_floppy.img";
 dd if="build/bootloader.bin" of="build/main_floppy.img" conv=notrunc;
 mcopy -i "build/main_floppy.img" "build/kernel.bin" "::kernel.bin"
+
+qemu-system-i386 -fda build/main_floppy.img
