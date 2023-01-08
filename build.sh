cc17478a5d9536c489a2e99879b714d3919d1d60
Matthias-pixel <vinc.gold01@gmail.com>
2023-01-08 21:49:56 +0100

    split run and debug scripts

---
+++ build.sh
@@ -0,0 +1,11 @@
+clear;
+set -e
+mkdir -p build
+
+nasm "src/bootloader/boot.asm" -f bin -o "build/bootloader.bin";
+nasm "src/kernel/main.asm" -f bin -o "build/kernel.bin";
+
+dd if="/dev/zero" of="build/main_floppy.img" bs=512 count=2880;
+mkfs.fat -F 12 -n "MYFIRSTOS" "build/main_floppy.img";
+dd if="build/bootloader.bin" of="build/main_floppy.img" conv=notrunc;
+mcopy -i "build/main_floppy.img" "build/kernel.bin" "::kernel.bin"
