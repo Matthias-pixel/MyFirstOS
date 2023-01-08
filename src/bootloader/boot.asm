d11d0b6c5370dc287ae730cc5bce843516412102
Matthias-pixel <vinc.gold01@gmail.com>
2023-01-08 21:21:07 +0100

    Refactored project for new tutorial

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,8 +1,40 @@
-[org 0x7c00]
+org 0x7c00
+bits 16
+; ---------------- Fat12 headers -------------------------------------
+
+jmp short start
+nop
+
+bdb_oem:                    db 'MSWIN4.1'           ; 8 bytes
+bdb_bytes_per_sector:       dw 512
+bdb_sectors_per_cluster:    db 1
+bdb_reserved_sectors:       dw 1
+bdb_fat_count:              db 2
+bdb_dir_entries_count:      dw 0E0h
+bdb_total_sectors:          dw 2880                 ; 2880 * 512 = 1.44MB
+bdb_media_descriptor_type:  db 0F0h                 ; F0 = 3.5" floppy disk
+bdb_sectors_per_fat:        dw 9                    ; 9 sectors/fat
+bdb_sectors_per_track:      dw 18
+bdb_heads:                  dw 2
+bdb_hidden_sectors:         dd 0
+bdb_large_sector_count:     dd 0
+
+; extended boot record
+ebr_drive_number:           db 0                    ; 0x00 floppy, 0x80 hdd, useless
+                            db 0                    ; reserved
+ebr_signature:              db 29h
+ebr_volume_id:              db 12h, 34h, 56h, 78h   ; serial number, value doesn't matter
+ebr_volume_label:           db 'MY FIRST OS'        ; 11 bytes, padded with spaces
+ebr_system_id:              db 'FAT12   '           ; 8 bytes
+
+; --------------- Bootloader main ------------------------------------
+start:
+    jmp main
 KERNEL_LOCATION equ 0x1000
 BOOT_DISK: db 0
 SECTOR_COUNT: db 2
-mov [BOOT_DISK], dl
+main:
+    mov [BOOT_DISK], dl
 initialize_registers:
     xor ax, ax                          
     mov es, ax
@@ -12,12 +44,12 @@ initialize_registers:
 
     mov bx, KERNEL_LOCATION
 load_sectors_from_disk:
-    mov ah, 2           ;; magic number
-    mov al, [SECTOR_COUNT] ;; number of sectors to read
-    mov ch, 0           ;; C ylinder number to start reading at
-    mov dh, 0           ;; H ead     number to start reading at
-    mov cl, 2           ;; S ector   number to start reading at
-    mov dl, [BOOT_DISK]   ;; drive number to read from
+    mov ah, 2                   ; magic number
+    mov al, [SECTOR_COUNT]      ; number of sectors to read
+    mov ch, 0                   ; C ylinder number to start reading at
+    mov dh, 0                   ; H ead     number to start reading at
+    mov cl, 2                   ; S ector   number to start reading at
+    mov dl, [BOOT_DISK]         ; drive number to read from
     int 0x13
 check_for_disk_load_error:
     jc disk_error
